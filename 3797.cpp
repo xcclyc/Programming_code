452fba30ec88f1938d0ccc043e3c482c5222a009
xcclyc <yiai_mincookook@qq.com>
2024-08-06 21:34:18 +0800

    Add files via upload

---
+++ 3797.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[100][100];
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < n;j++)
+		{
+			a[i][j] = 
+		}
+	}
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < n;j++)
+		{
+			cout << a[i][j] << " ";
+		}
+	}
+}
